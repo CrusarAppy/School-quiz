0b51fdcf127cfb657868ce1c9f9e74d08a802f4b
CrusarAppy <bhandariapar95@gmail.com>
2024-01-07 09:38:30 +0545

    completed project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,172 @@
 // List of Questions and Answers
 
 var questions = [
+  {
+    "prompt": "कुन शहरमा पशुपतिनाथ मन्दिर, एक विश्व सांस्कृतिक धरोहर स्थल, अवस्थित छ?",
+    "options": ["काठमाडौं", "भक्तपुर", "ललितपुर", "पोखरा"],
+    "answer": "काठमाडौं"
+  },
+  {
+    "prompt": "कुन शहरमा स्वयम्भूनाथ स्तूप स्थित छ, जुनले यसलाई एक विश्व सांस्कृतिक धरोहर बनाएको छ?",
+    "options": ["काठमाडौं", "ललितपुर", "भक्तपुर", "पोखरा"],
+    "answer": "काठमाडौं"
+  },
+  {
+    "prompt": "कुन शहरमा लुम्बिनी, बुद्धको जन्मस्थल, अवस्थित छ?",
+    "options": ["काठमाडौं", "पोखरा", "ललितपुर", "रुपन्देही"],
+    "answer": "रुपन्देही"
+  },
+  {
+    "prompt": "Which organ is responsible for pumping blood throughout the body?",
+    "options": ["Lungs", "Liver", "Heart", "Kidneys"],
+    "answer": "Heart"
+  },
+  {
+    "prompt": "Where is the smallest bone in the human body located?",
+    "options": ["Ear", "Hand", "Foot", "Nose"],
+    "answer": "Ear"
+  },
+  {
+    "prompt": "What is the largest organ of the human body?",
+    "options": ["Heart", "Liver", "Brain", "Skin"],
+    "answer": "Skin"
+  },
+  {
+    "prompt": "What is the primary function of the respiratory system?",
+    "options": ["Digestion", "Circulation", "Breathing", "Excretion"],
+    "answer": "Breathing"
+  },
+  {
+    "prompt": "Which bone protects the brain?",
+    "options": ["Skull", "Spine", "Ribcage", "Pelvis"],
+    "answer": "Skull"
+  },
+  {
+    "prompt": "Where is the femur bone located?",
+    "options": ["Arm", "Leg", "Spine", "Skull"],
+    "answer": "Leg"
+  },
+  {
+    "prompt": "Which gland produces insulin?",
+    "options": ["Pancreas", "Thyroid", "Adrenal", "Pituitary"],
+    "answer": "Pancreas"
+  },
+  {
+    "prompt": "Which technology is commonly associated with the development of self-driving cars?",
+    "options": ["Blockchain", "Artificial Intelligence", "Virtual Reality", "Quantum Computing"],
+    "answer": "Artificial Intelligence"
+  },
+  {
+    "prompt": "Which renewable energy source is considered one of the most promising for the future due to its efficiency and sustainability?",
+    "options": ["Solar", "Wind", "Hydro", "Geothermal"],
+    "answer": "Solar"
+  },
+  {
+    "prompt": "What is the name of the SpaceX rocket designed for interplanetary travel and potential colonization of Mars?",
+    "options": ["Falcon 9", "Starship", "Dragon", "Falcon Heavy"],
+    "answer": "Starship"
+  },
+  {
+    "prompt": "What is the purpose of the Human Genome Project?",
+    "options": ["Space exploration", "Mapping the human brain", "Understanding human genetic code", "Developing artificial organs"],
+    "answer": "Understanding human genetic code"
+  },
+  {
+    "prompt": "What is the primary purpose of an operating system?",
+    "options": ["Data storage", "User interface", "Hardware management", "Internet browsing"],
+    "answer": "Hardware management"
+  },
+  {
+    "prompt": "Which programming language is often used for developing web applications?",
+    "options": ["Java", "Python", "JavaScript", "C++"],
+    "answer": "JavaScript"
+  },
+  {
+    "prompt": "What does the acronym HTML stand for?",
+    "options": ["Hypertext Markup Language", "Hyperlink and Text Markup Language", "High-Level Text Management Language", "Home Tool Markup Language"],
+    "answer": "Hypertext Markup Language"
+  },
+  {
+    "prompt": "Which data structure follows the Last In, First Out (LIFO) principle?",
+    "options": ["Queue", "Stack", "Linked List", "Array"],
+    "answer": "Stack"
+  },
+  {
+    "prompt": "Which networking protocol is commonly used for transferring files over the internet?",
+    "options": ["HTTP", "FTP", "SMTP", "UDP"],
+    "answer": "FTP"
+  },
+  {
+    "prompt": "What is the term for a software program that replicates itself and spreads to other computers?",
+    "options": ["Virus", "Trojan Horse", "Worm", "Spyware"],
+    "answer": "Worm"
+  },
+  {
+    "prompt": "Which programming language is commonly used for developing mobile applications?",
+    "options": ["Java", "Python", "Swift", "C#"],
+    "answer": "Swift"
+  },
+  {
+    prompt: "In which district is Fungfung waterfall?",
+    options: ["Nuwakot", "Dolpa", "Palpa", "Kaskis"],
+    answer: "Nuwakot"
+},
+{
+  prompt: "How much of the Heritage of Nepal is to be celebrated in the World Heritage Site?",
+  options: ["11", "10", "12", "14"],
+  answer: "10"
+},
+{
+  "prompt": "In which year did Nepal receive its first World Heritage Site designation?",
+  "options": ["1976", "1980", "1987", "1992"],
+  "answer": "1979"
+},
+{
+  "prompt": "Which of the following is not a World Heritage Site in Nepal?",
+  "options": ["Lumbini", "Sagarmatha National Park", "Pashupatinath Temple", "Annapurna Conservation Area"],
+  "answer": "Annapurna Conservation Area"
+},
+{
+  "prompt": "What is the name of the ancient city in the Kathmandu Valley that is a World Heritage Site?",
+  "options": ["Bhaktapur", "Patan", "Kathmandu", "Kirtipur"],
+  "answer": "Bhaktapur"
+},
+{
+  "prompt": "Which river basin is home to the Chitwan National Park, a World Heritage Site in Nepal?",
+  "options": ["Gandaki", "Koshi", "Bagmati", "Rapti"],
+  "answer": "Rapti"
+},
+{
+  "prompt": "How many cultural heritage sites are there among the World Heritage Sites in Nepal?",
+  "options": ["5", "6", "7", "8"],
+  "answer": "7"
+},
+{
+  "prompt": "Which mountain range is not represented by a World Heritage Site in Nepal?",
+  "options": ["Himalayas", "Annapurna", "Langtang", "Manaslu"],
+  "answer": "Manaslu"
+},
+{
+  "prompt": "In which category does Mount Everest, a part of Sagarmatha National Park, fall under the World Heritage designation?",
+  "options": ["Cultural", "Mixed", "Natural", "Industrial"],
+  "answer": "Mixed"
+},
+
+{
+  prompt: " Who was the Zero inventor?",
+  options: ["Ram Mohan Bhatt", "Karam Das Chhachad", "Arya Bhatt", "Adis Mahatto"],
+  answer: "Arya Bhatt"
+},
+{
+  prompt: " What is the first person climbing Mount Everest without both hands?",
+  options: ["Sudarshan Gautam", "Ram Chandra Parajuli", "Himal Sherpa", "Sugam Dhungana"],
+  answer: "Sudarshan Gautam"
+},
+{
+  prompt: "What animal is has three hearts and blue blood?",
+  options: ["Sheep", "Whale", "Octopus", "Zebra"],
+  answer: "Octopus"
+},
     {
         prompt: "Inside which HTML element do we put the JavaScript?",
         options: ["<javascript>", "<js>", "<script>", "<scripting>"],
